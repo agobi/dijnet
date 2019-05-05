f3d76f0e3e72ed75ad75f3db760036a58b7f271a
madeinjapan <morris86@gmail.com>
2019-05-06 01:12:35 +0200

    IndexError_#1:
     - extend ini file
     - update script to the new website
     - fixed the errors
     - add onclickParser function

--- dijnet.py
+++ dijnet.py
@@ -1,24 +1,35 @@
-#!/usr/bin/env python2
+#!/usr/bin/env python3
 # vim: set fileencoding=utf-8 :
 
-import ConfigParser
+import configparser
 import cgi
 import sys
 
 import mechanicalsoup
 
-config = ConfigParser.SafeConfigParser()
+def onclickParser(onclick):
+    linkStartIndex = onclick.find('\'') + 1
+    onclickLink = onclick[linkStartIndex:-2]
+    return onclickLink
+
+config = configparser.ConfigParser()
 config.read("dijnet.ini")
 
-br = mechanicalsoup.StatefulBrowser(soup_config={'features': 'lxml'})
-br.open("http://dijnet.hu")
+username = config.get('global', 'username')
+password = config.get('global', 'password')
+url = config.get('global', 'url')
+loginPage = url + config.get('global', 'login')
+logoutPage = url + config.get('global', 'logout')
 
-br.select_form("#login-form")
-br["username"] = config.get("global", "username")
-br["password"] = config.get("global", "password")
+br = mechanicalsoup.StatefulBrowser(soup_config={'features': 'lxml'})
+br.open(loginPage)
+br.select_form('form[action="/ekonto/login/login_check_password"]')
+br["username"] = username
+br["password"] = password
 br.submit_selected()
+
 if not br.get_current_page().find(string="Ügyfél: "):
-    print >> sys.stderr, "A bejelentkezes nem sikerult."
+    print("A bejelentkezes nem sikerult.", file=sys.stderr)
     sys.exit(1)
 
 br.follow_link(link_text=u'Számlakeresés')
@@ -28,20 +39,32 @@ br.select_form(nr=0)
 br.submit_selected()
 page = br.get_current_page()
 
-links = [t.td.a for t in page.select('table.szamla_table')[0].find_all('tr')]
+links = []
+
+for t in page.select('table.szamla_table')[0].find_all('tr'):
+    links.append(url + onclickParser(t.td['onclick']))
 
 for link in links:
-    br.follow_link(link)
+    br.open(link)
     br.follow_link(link_text=u'Letöltés')
+    page = br.get_current_page()
+    messages = page.find_all('div', class_="xt_link_cell__download")
+    linkText = ''
 
-    download_link = br.find_link(link_text=u'\xa0Számla nyomtatható verziója (PDF) - Hiteles számla')
+    for message in messages:
+        if 'Hiteles számla' in message.text:
+            linkText = message.text
+    
+    download_link = br.find_link(link_text=linkText)
 
     data = br.session.get(br.absolute_url(download_link['href']))
     header = data.headers['Content-Disposition']
     value, params = cgi.parse_header(header)
-    with open(params['filename'], "w") as f:
+    with open(params['filename'], "wb") as f:
         f.write(data.content)
-    print params['filename']
+    print(params['filename'])
 
     return_link = br.get_current_page().select('a.xt_link__title')
     br.follow_link(return_link[0])
+
+br.open(logoutPage)
