851609d8f16092463429e080300d638928383356
Szabó Péter <1254135+szabopeter@users.noreply.github.com>
2019-10-27 19:29:42 +0100

    PEP8

--- dijnet.py
+++ dijnet.py
@@ -20,53 +20,53 @@ config.read("dijnet.ini")
 username = config.get('global', 'username')
 password = config.get('global', 'password')
 url = config.get('global', 'url')
-loginPage = url + config.get('global', 'login')
-logoutPage = url + config.get('global', 'logout')
+login_page = url + config.get('global', 'login')
+logout_page = url + config.get('global', 'logout')
 
-br = mechanicalsoup.StatefulBrowser(soup_config={'features': 'lxml'})
-br.open(loginPage)
-br.select_form('form[action="/ekonto/login/login_check_password"]')
-br["username"] = username
-br["password"] = password
-br.submit_selected()
+browser = mechanicalsoup.StatefulBrowser(soup_config={'features': 'lxml'})
+browser.open(login_page)
+browser.select_form('form[action="/ekonto/login/login_check_password"]')
+browser["username"] = username
+browser["password"] = password
+browser.submit_selected()
 
-if not br.get_current_page().find(string="Ügyfél: "):
+if not browser.get_current_page().find(string="Ügyfél: "):
     print("A bejelentkezes nem sikerult.", file=sys.stderr)
     sys.exit(1)
 
-br.follow_link(link_text=u'Számlakeresés')
-assert br.get_current_page().title.text == u'DÍJNET'
+browser.follow_link(link_text=u'Számlakeresés')
+assert browser.get_current_page().title.text == u'DÍJNET'
 
-br.select_form(nr=0)
-br.submit_selected()
-page = br.get_current_page()
+browser.select_form(nr=0)
+browser.submit_selected()
+page = browser.get_current_page()
 
 links = []
 
-for t in page.select('table.szamla_table')[0].find_all('tr'):
-    links.append(url + onclick_parser(t.td['onclick']))
+for row in page.select('table.szamla_table')[0].find_all('tr'):
+    links.append(url + onclick_parser(row.td['onclick']))
 
 for link in links:
-    br.open(link)
-    br.follow_link(link_text=u'Letöltés')
-    page = br.get_current_page()
+    browser.open(link)
+    browser.follow_link(link_text=u'Letöltés')
+    page = browser.get_current_page()
     messages = page.find_all('div', class_="xt_link_cell__download")
-    linkText = ''
+    link_text = ''
 
     for message in messages:
         if 'Hiteles számla' in message.text:
-            linkText = message.text
+            link_text = message.text
     
-    download_link = br.find_link(link_text=linkText)
+    download_link = browser.find_link(link_text=link_text)
 
-    data = br.session.get(br.absolute_url(download_link['href']))
+    data = browser.session.get(browser.absolute_url(download_link['href']))
     header = data.headers['Content-Disposition']
     value, params = cgi.parse_header(header)
     with open(params['filename'], "wb") as f:
         f.write(data.content)
     print(params['filename'])
 
-    return_link = br.get_current_page().select('a.xt_link__title')
-    br.follow_link(return_link[0])
+    return_link = browser.get_current_page().select('a.xt_link__title')
+    browser.follow_link(return_link[0])
 
-br.open(logoutPage)
+browser.open(logout_page)
